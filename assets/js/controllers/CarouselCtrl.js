91ad7f470fcdbbf42206842c5322f86ad04bd1f6
leviross <levross@gmail.com>
2015-02-23 16:47:47 -0800

    fixed slider bug

--- assets/js/controllers/CarouselCtrl.js
+++ assets/js/controllers/CarouselCtrl.js
@@ -1,9 +1,9 @@
 watchApp.controller('CarouselCtrl',['$scope',function($scope){
   $scope.myInterval = 3000;
   $scope.slides = [
-    {image:'/images/luxury4.jpg'},
-    {image:'/images/luxury2.JPG'},
-    {image:'/images/luxury3.jpg'}
+    {image:'/images/luxury4.jpg',text:'item 1'},
+    {image:'/images/luxury2.JPG',text:'item 2'},
+    {image:'/images/luxury3.jpg',text:'item 3'}
   ];
 
 }]);
